aa04700119a691ce9ae895663907f07f90e4a9b0
spdevuk <spdevuk@gmail.com>
2018-12-17 13:02:48 +0000

    Adding prototypeChain and updating README

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ Just some basic notes for OOP stuff, testing out typescript a little bit plus it
 Read .ts files for ES6, .js files for ES5
 
 
-Use better comments to see colourful comments [VSCode](https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments) |  [Jetbrains](https://plugins.jetbrains.com/plugin/10850-better-comments)
-
-? means technical info, * means general/non technical speak info/ ! means important
+folders:
+- class: the keyword 'class' and what it does
+- new: the keyword 'new' when used to create a class
+- this: keyword 'this'
+- prototypeChain: things to do with the prototype chain, explains why a method can be used on an object even if that object does not have that method directly 
 
 
 TL;DR:
@@ -14,10 +16,16 @@ TL;DR:
 prototype is on the class 
 
 
+why does `typeof String` return "function"?:
+(also the same with Object, Array etc)
+Because String is a function object, not just a normal object. This is so it can hold all of the methods for use on only strings in javascript.  If you `console.log(String.prototype)` a list of all string methods will show up, but 
+you can even add to this. 
+
+Add something such as `String.protoype.log = console.log`
+now you can call console.log as a method on any string. `"asdf".log("hello")` will log "hello" to the console.
+
+And since String begins with a capital letter we can assume you can also use the keyword `new` on it, which you can. eg `const hw = new String("hello, world")` will return an object with 11 keys, from 0 to 11 and a length of 11. `{"Hello, world", length: 11}` you can then call `hw[0]` and see "h" or `hw.length` to return 11.
+
+To you can safely assume any string in JS is just part of the String "class", it has access to all methods inside String, but in reality every time you look up a method on a string it will look for the method on that string and not find it, then look up the `__proto__` on that string, and find every method for String there. All of these methods actually belong in String.prototype.
 
 
-folders:
-- class: the keyword 'class' and what it does
-- new: the keyword 'new' when used to create a class
-- this: keyword 'this'
-- prototypeChain: things to do with the prototype chain
--- prototypeChain/prototypeChain.ts
+++ prototypeChain/prototypeChain.ts
@@ -1,9 +1,52 @@
-// when starting the javascript runtime it automatically adds a function/object with the label Object:
+// not everything in javascript is an object, but things like strings, functions etc are "JS-objects" that are a bit different and have hidden(?) methods such as __proto__,
+// but aren't the same as actually being an object, eg a string is immutable and a string would be considered a "String object" not a normal object
+
+
+// when starting the javascript runtime it automatically adds a function/object with the label Object
+// every object in javascript has a __proto__ key, and some others, if you console.log({}) in chrome devtools you can look through them
+// so no matter how deep you go or where an object is, you can always look for __proto__ or hasOwnProperty etc.
+
+// because other things such as strings, arrays are also just objects and you can see their __proto__ inside the chrome devtools,
+// eg  console.log([]) has a __proto__ that shows the methods available to all arrays, with another __proto__ inside that shows the __proto__ of an object.
+// const str = "hi";  console.log(str.__proto__) will display all methods of a string, again it has another __proto__ inside it
+
+
+// FUNCTIONS HAVE .prototype and __proto__, OBJECTS ONLY HAVE __proto__
+
+// Function.prototype has things such as toString(), bind, length etc.
+// (similar to how you would look for methods in a class, because a class is just a function after all)
+// You would only see the prototype if you ever used the 'new' Keyword before a function.
+// this prototype also has a __proto__ inside it, things like toString are inside
 
 const obj = {
   num: 3
 };
 
 obj.num; // 3
-obj.hasOwnProperty("num"); // we find this on __proto__ on the obj, it is a function inside __proto__
+// obj.hasOwnProperty will lookup obj in global memory. it will find obj, but not hasOwnProperty on the obj.
+// after not finding it on obj it will look into __proto__ on to obj, then when called it will check that obj has the property num
+obj.hasOwnProperty("num");
 console.log(Object.prototype);
+
+
+function multiplyBy2(num) {
+  return num * 2;
+}
+
+multiplyBy2.toString();
+// looks for multiplyBy2 in global memory, then because we are using dot notation it will be looking at the "object" part of the function,
+// it will look inside the multiplyBy2.__proto__, which is linked to Function.prototype, inside Function.prototype there will be toString.
+
+// Function.prototype is for ALL functions.
+
+// lookup on multiplyBy2 using dot notation -> can't find it on the object -> look in __proto__ on the function, which links to Function.prototype
+// Function.prototype has the method, we use it.
+
+
+multiplyBy2.hasOwnProperty("score");
+//  lookup on multiplyBy2 using dot notation -> can't find it on the object -> look in __proto__ on the function, which links to Function.prototype -> Function.prototype has a __proto__,
+//  that __proto__ of Function.prototype links to Object.prototype, where we find anything that works on any object.
+
+
+// multiplyBy2.increase();
+// this would do the same as looking for hasOwnProperty, looking all the way up the chain until we reach the __proto__ of Object, which is null. and it fails because it does not exist.
